18c39795328a18324d254ea75f283c036a085340
Avila <javila6@gatech.edu>
2016-04-17 21:37:35 -0400

    Got zone analysis working

--- www/js/practice/services.js
+++ www/js/practice/services.js
@@ -26,22 +26,24 @@ angular.module('practice')
     }).then(function (response) {
       // Init the data array
       for (var pos in TARGET_ZONES) {
-        for (var type in SHOT_TYPES) {
+        dataArr[TARGET_ZONES[pos]] = new Array(Object.keys(SHOT_TYPES).length).fill(0);
+        for (var type in dataArr[0]) {
           dataArr[TARGET_ZONES[pos]][type] = {"Success":0, "Total":0, "Percent":0};
         }
       }
       //Compute %'s
       for (var i = 0; i < response.data.length; i++) {
         var zone = response.data[i]["Zone"];
-        var type = response.data[i]["Type"];
-        console.log("zone: " + zone + " type: " + type);
-        // dataArr[zone]["Success"] += response.data[i]["Made"] * response.data[i]["Count(*)"];
-        // dataArr[zone]["Total"] += response.data[i]["Count(*)"];
+        var type = response.data[i]["Type"] - 1; //Types don't index from 0, so reduce by 1.
+        dataArr[zone][type]["Success"] += response.data[i]["Made"] * response.data[i]["Count(*)"];
+        dataArr[zone][type]["Total"] += response.data[i]["Count(*)"];
       }
-      // for (var i = 0; i < dataArr.length; i++) {
-      //   dataArr[i]["Percent"] = Math.round(100 * (dataArr[i]["Success"] / dataArr[i]["Total"]));
-      //   console.log("ZONE: " + i + " PERCENT: " + dataArr[i]["Percent"]);
-      // }
+      for (var i = 0; i < dataArr.length; i++) {
+        for (var type in dataArr[0]) {
+          dataArr[i][type]["Percent"] = Math.round(100 * (dataArr[i][type]["Success"] / dataArr[i][type]["Total"]));
+          console.log("ZONE: " + i + " TYPE: " + type + " PERCENT: " + dataArr[i][type]["Percent"]);
+        }
+      }
     });
 
     // END UGLY TESTING
