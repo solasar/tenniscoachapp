9f70257f7004c30c43cf4e326ed3d752c3340ff6
Avila <javila6@gatech.edu>
2016-04-17 21:38:18 -0400

    Put zonestat in its rightful place

--- www/js/practice/services.js
+++ www/js/practice/services.js
@@ -13,39 +13,6 @@ angular.module('practice')
     /////////////////////
     // BEGIN UGLY TESTING
 
-    var dataArr = [];
-    /* var uid = window.localStorage.getItem(STORAGE_KEYS.userId);
-     console.log("STATS TEST: " + uid); */
-    $http.post('http://54.164.54.3/getZone', {username: "alpha"})
-      .success(function (data, status, headers, config){
-        var shots = data.response;
-        console.log("post success");
-        // Compute percentages per section.
-      }).error(function (data, status, headers, config){
-      console.log("post failed " + status);
-    }).then(function (response) {
-      // Init the data array
-      for (var pos in TARGET_ZONES) {
-        dataArr[TARGET_ZONES[pos]] = new Array(Object.keys(SHOT_TYPES).length).fill(0);
-        for (var type in dataArr[0]) {
-          dataArr[TARGET_ZONES[pos]][type] = {"Success":0, "Total":0, "Percent":0};
-        }
-      }
-      //Compute %'s
-      for (var i = 0; i < response.data.length; i++) {
-        var zone = response.data[i]["Zone"];
-        var type = response.data[i]["Type"] - 1; //Types don't index from 0, so reduce by 1.
-        dataArr[zone][type]["Success"] += response.data[i]["Made"] * response.data[i]["Count(*)"];
-        dataArr[zone][type]["Total"] += response.data[i]["Count(*)"];
-      }
-      for (var i = 0; i < dataArr.length; i++) {
-        for (var type in dataArr[0]) {
-          dataArr[i][type]["Percent"] = Math.round(100 * (dataArr[i][type]["Success"] / dataArr[i][type]["Total"]));
-          console.log("ZONE: " + i + " TYPE: " + type + " PERCENT: " + dataArr[i][type]["Percent"]);
-        }
-      }
-    });
-
     // END UGLY TESTING
     /////////////////////
 
--- www/js/statistic/services.js
+++ www/js/statistic/services.js
@@ -2,7 +2,7 @@ angular.module('statistic')
 .service('StatisticService', function ($http, $ionicPopup, STORAGE_KEYS, SHOT_POSITIONS, SHOT_TYPES) {
 
   var heatMapValues = function() {
-    // NOTE: you should only return the post response here, don't do anything else 
+    // NOTE: you should only return the post response here, don't do anything else
     // begin ugly testing stuff
     var dataArr = [];
     /* var uid = window.localStorage.getItem(STORAGE_KEYS.userId);
@@ -35,10 +35,43 @@ angular.module('statistic')
   };
 
   var zoneStatValues = function() {
-    var retArr = [];
-    return retArr;
+    // NOTE: you should only return the post response here, don't do anything else
+    var dataArr = [];
+    /* var uid = window.localStorage.getItem(STORAGE_KEYS.userId);
+     console.log("STATS TEST: " + uid); */
+    $http.post('http://54.164.54.3/getZone', {username: "alpha"})
+      .success(function (data, status, headers, config){
+        var shots = data.response;
+        console.log("post success");
+        // Compute percentages per section.
+      }).error(function (data, status, headers, config){
+      console.log("post failed " + status);
+    }).then(function (response) {
+      // Init the data array
+      for (var pos in TARGET_ZONES) {
+        dataArr[TARGET_ZONES[pos]] = new Array(Object.keys(SHOT_TYPES).length).fill(0);
+        for (var type in dataArr[0]) {
+          dataArr[TARGET_ZONES[pos]][type] = {"Success":0, "Total":0, "Percent":0};
+        }
+      }
+      //Compute %'s
+      for (var i = 0; i < response.data.length; i++) {
+        var zone = response.data[i]["Zone"];
+        var type = response.data[i]["Type"] - 1; //Types don't index from 0, so reduce by 1.
+        dataArr[zone][type]["Success"] += response.data[i]["Made"] * response.data[i]["Count(*)"];
+        dataArr[zone][type]["Total"] += response.data[i]["Count(*)"];
+      }
+      for (var i = 0; i < dataArr.length; i++) {
+        for (var type in dataArr[0]) {
+          dataArr[i][type]["Percent"] = Math.round(100 * (dataArr[i][type]["Success"] / dataArr[i][type]["Total"]));
+          console.log("ZONE: " + i + " TYPE: " + type + " PERCENT: " + dataArr[i][type]["Percent"]);
+        }
+      }
+    });
+
+    return dataArr;
   };
-  
+
   return {
     heatMap: heatMapValues,
     zoneStat: zoneStatValues
