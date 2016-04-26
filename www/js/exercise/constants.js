0f7940f840a0539030a0f1608e6a52b437a163b3
Keun S. Lee <klee368@mail.gatech.edu>
2016-04-26 15:23:15 -0400

    Add in exercise module again

---
+++ www/js/exercise/constants.js
@@ -0,0 +1,34 @@
+angular.module('exercise')
+
+.constant ('LEFT_SECTION', {
+  1: 'TLLL',
+  2: 'TLLR',
+  3: 'TLRL',
+  9: 'MLLL',
+  10: 'MLLR',
+  11: 'MLRL',
+  17: 'BLLL',
+  18: 'BLLR',
+  19: 'BLRL'
+})
+
+.constant ('CENTER_SECTION', {
+  4: 'TLRR',
+  5: 'TRLL',
+  12: 'MLRR',
+  13: 'MRLL',
+  20: 'BLRR',
+  21: 'BRLL'
+})
+
+.constant ('RIGHT_SECTION', {
+  6: 'TRLR',
+  7: 'TRRL',
+  8: 'TRRR',
+  14: 'MRLR',
+  15: 'MRRL',
+  16: 'MRRR',
+  22: 'BRLR',
+  23: 'BRRL',
+  24: 'BRRR'
+})
